1a8f0d05eba95f63b9c569a47f0e6e281c143d72
Mahdi Amereh <mahdieightt@gmail.com>
2024-02-13 13:45:26 +0330

    feat: add user lang

---
+++ lang/en/user.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'enums' => [],
+    'messages' => [
+        'users_successfully_found' => 'User list Successfully founded',
+    ],
+    'validations' => [],
+    'errors' => [],
+];
